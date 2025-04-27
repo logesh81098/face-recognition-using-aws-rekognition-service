259e62fc12cb5cc5a21496a7d44abe92091187f4
Logesh Shanmugavel <logesh81098@gmail.com>
2025-04-27 18:44:15 +0530

    Created IAM Role to GetObject in S3, PutItems in DynamoDB, IndexFaces in Rekognition

--- module/iam-role/main.tf
+++ module/iam-role/main.tf
@@ -74,3 +74,95 @@ resource "aws_iam_role_policy_attachment" "rekognition-collection-id-role-policy
   role = aws_iam_role.rekognition-collection-id-role.id
   policy_arn = aws_iam_policy.rekognition-collection-id-create-policy.arn
 }
+
+
+#####################################################################################################################################
+#                                                     IAM Role
+#####################################################################################################################################
+
+#IAM Role for Lambda to with permission to get objects from S3, process it with Lambda function, create faceprints using Rekognition service and store faceprints in DynamoDb table
+
+resource "aws_iam_role" "rekognition-faceprints" {
+  name = "rekognition-faceprints"
+  description = "IAM Role for Lambda to with permission to get objects from S3, process it with Lambda function, create faceprints using Rekognition service and store faceprints in DynamoDb table"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+}  
+EOF
+}
+
+#####################################################################################################################################
+#                                                     IAM Policy
+#####################################################################################################################################
+
+#IAM Policy with permission to get objects from S3, process it with Lambda function, create faceprints using Rekognition service and store faceprints in DynamoDb table
+
+resource "aws_iam_policy" "rekognition-faceprints-policy" {
+  name = "rekognition-faceprints-policy"
+  description = "IAM Policy with permission to get objects from S3, process it with Lambda function, create faceprints using Rekognition service and store faceprints in DynamoDb table"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "CloudWatchLogGroup",
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+        ],
+            "Resource": "arn:aws:logs:*:*:*"
+        },
+        {
+            "Sid": "S3Bucket",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:HeadObject"
+            ],
+            "Resource": "arn:aws:s3:::individuals-source-image/*"
+
+        },
+        {
+            "Sid": "DynamoDB",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:PutItem"
+            ],
+            "Resource": "arn:aws:dynamodb:*:*:table/faceprints-table"
+        },
+        {
+            "Sid": "RekognitionCollectionID",
+            "Effect": "Allow",
+            "Action": [
+                "rekognition:IndexFaces"
+            ],
+            "Resource": "arn:aws:rekognition:*:*:collection/*"
+        }
+    ]
+}  
+EOF
+}
+
+
+#####################################################################################################################################
+#                                                     IAM Role Policy Attachment
+#####################################################################################################################################
+
+#Attaching IAM Role and Policy
+
+resource "aws_iam_role_policy_attachment" "faceprints-role-policy" {
+  role = aws_iam_role.rekognition-faceprints.id
+  policy_arn = aws_iam_policy.rekognition-faceprints-policy.arn
+}
